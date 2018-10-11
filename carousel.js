68ddd2100b2b15f52b1be6d8b3fea0fc40c1422c
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2018-10-11 13:19:58 +0200

    carousel set style

--- carousel.js
+++ carousel.js
@@ -1,6 +1,12 @@
 class Carousel{
-  constructor(element){
+  
+  constructor(element, options = {}){
     this.element = element;
+
+    this.options = Object.assign({}, {
+      slidesToScroll: 1,
+      slidesVisible: 3
+    }, options);
 
     /**
       * Petit hack
@@ -8,21 +14,32 @@ class Carousel{
       * sera executé lors de l'action. Va le convertir en tableau au lieu de la node liste
       * Cette ligne de code est placée au début pour respecter la hierarchie des element.
     **/
-    this.children = [].slice.call(element.children);
+    let children = [].slice.call(element.children);
+
+    
 
     /* Ajoute la class div.carousel */
     let divRoot = this.createDivNClass('carousel');
-    let divContainer = this.createDivNClass('carousel-container');
+    this.divContainer = this.createDivNClass('carousel-container');
     divRoot.appendChild(divContainer);
     this.element.appendChild(divRoot);
 
+    
 
     /* Mettre les slides "enfants" dans le divContainer */
-    this.children.forEach((elementChild) => {
+    this.items = children.Map((elementChild) => {
       let item = this.createDivNClass('carousel-item');
+      item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%";
       item.appendChild(elementChild);
-      divContainer.appendChild(item);
+      this.divContainer.appendChild(item);
+
+      return item;
     });
+  }
+
+  setStyle(){
+    let ratio = this.children.length / this.options.slidesVisible;
+    this.divContainer.style.width = (ratio * 100) + '%';
   }
 
   createDivNClass(className){
@@ -34,5 +51,43 @@ class Carousel{
 
 
 document.addEventListener('DOMContentLoaded', function(){
-  new Carousel(document.querySelector('#carousel'));
+  new Carousel(document.querySelector('#carousel'), {
+    slidesToScroll: 1,
+    slidesVisible: 3
+  });
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
